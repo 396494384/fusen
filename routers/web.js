533ea9e109e84ae8bff25ad02a62b6a42224e003
Your Name <you@example.com>
2019-07-23 21:50:31 +0800

    fusen

--- routers/web.js
+++ routers/web.js
@@ -12,13 +12,14 @@ const Category = require('../models/Category');
 const Product = require('../models/Product');
 
 let public = null;
-router.use((req, res, next)=>{
-  Public.find().then(data=>{
+router.use((req, res, next) => {
+  Public.find().then(data => {
     public = data[0];
     next();
   })
 })
 
+// 首页
 router.get('/', (req, res) => {
   res.render('index', {
     public: public
@@ -30,30 +31,21 @@ router.get('/index', (req, res) => {
   })
 })
 
+
+// 新闻
 router.get('/news', (req, res) => {
   New.findOne({
     type: "顶部显示",
     status: true
-  }).then(data=>{
+  }).then(data => {
     let _big = null;
-    if(data){
+    if (data) {
       _big = data;
-      // _big.content = data.content.replace(/<[^>]+>/g,"");
-      _big.content = data.content.replace(/(\n)/g, "");  
-      _big.content = _big.content.replace(/(\t)/g, "");  
-      _big.content = _big.content.replace(/(\r)/g, "");  
-      _big.content = _big.content.replace(/<\/?[^>]*>/g, "");  
-      _big.content = _big.content.replace(/\s*/g, "");
-      _big.content = _big.content.replace(/&nbsp;/g, " ");
     }
     New.find({
       type: "列表显示",
       status: true
-    }).then(data=>{
-      console.log(data)
-      data.forEach(i=>{
-        i.content = "xxx";
-      })
+    }).then(data => {
       res.render('news', {
         public: public,
         big: _big,
@@ -61,8 +53,69 @@ router.get('/news', (req, res) => {
       })
     })
   })
-  
 })
 
+
+// 服务网点
+router.get('/branch', (req, res) => {
+  let _branch = [];
+  City.find().then(data => {
+    if (data.length > 0) {
+      let _citys = data;
+      Store.find({
+        status: true
+      }).then(data => {
+        if (data.length > 0) {
+          _citys.forEach(i => {
+            if (i.number > 0) {
+              _branch.push({
+                city: i.name,
+                cityId: i.id,
+                items: []
+              })
+            }
+          })
+          _branch.forEach(i => {
+            data.forEach(j => {
+              if (j.city == i.cityId) {
+                i.items.push(j)
+              }
+            })
+          })
+          res.render('branch', {
+            public: public,
+            branch: _branch
+          })
+        } else {
+          res.render('branch', {
+            public: public,
+            branch: _branch
+          })
+        }
+      })
+    } else {
+      res.render('branch', {
+        public: public,
+        branch: _branch
+      })
+    }
+  })
+})
+
+
+// 招聘
+router.get('/recruit', (req, res) => {
+  let _type = req.query.type == 2 ? 2 : 1;
+  Recruit.find({
+    type: _type == 1 ? "社会招聘" : "校园招聘",
+    status: true
+  }).then(data => {
+    res.render('recruit', {
+      public: public,
+      recruit: data,
+      type: _type
+    })
+  })
+})
 
 module.exports = router;
